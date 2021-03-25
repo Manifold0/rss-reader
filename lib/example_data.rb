131477f2b0189e15c97a6ecea8e40a9f6d0bb476
ncox <ncox@topsailtech.com>
2021-03-25 17:35:41 -0400

    Apparently this was symbolizing these strings. Use fat arrow instead.

--- lib/example_data.rb
+++ lib/example_data.rb
@@ -2,10 +2,10 @@ class Example
   # Assume that URLs come stored in an array, in case there are multiple URLs
   def data
     {
-      "The Apology Line":   ["https://rss.art19.com/apology-line"],
-      "The New York Times": ["http://rss.art19.com/the-daily", "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"],
-      "CNN":                ["http://rss.cnn.com/rss/cnn_topstories.rss"],
-      "LifeHacker":         ["https://lifehacker.com/rss"]
+      "The Apology Line" =>   ["https://rss.art19.com/apology-line"],
+      "The New York Times" => ["http://rss.art19.com/the-daily", "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"],
+      "CNN" =>                ["http://rss.cnn.com/rss/cnn_topstories.rss"],
+      "LifeHacker" =>         ["https://lifehacker.com/rss"]
     }
   end
 end

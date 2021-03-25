28a0254d7f1334a1c8444aed6c2d65e791414fef
ncox <ncox@topsailtech.com>
2021-03-25 16:01:02 -0400

    Spruce up explanatory comments

--- lib/rss_reader.rb
+++ lib/rss_reader.rb
@@ -2,6 +2,8 @@ class RssReader
   require 'rss' # basic RSS library to parse the feed
   require 'open-uri' # basic wrapper library for HTTP to get feed data
 
+  # rss_dictionary: hash of company names and RSS urls
+  # days_inactive: integer number of days
   def initialize(rss_dictionary, days_inactive)
     @rss_dictionary = rss_dictionary
     @days_inactive = days_inactive
@@ -11,6 +13,7 @@ class RssReader
 
   end
 
+  # Parse RSS feed and pull pubDates out of each item
   def rss_feed_pub_dates(feed_url)
     pub_dates = []
     open(feed_url) do |rss|
@@ -22,9 +25,11 @@ class RssReader
     pub_dates
   end
 
-  def includes_inactive?(pub_dates, inactivity_window)
-    pub_dates.each_with_index do |date, index|
+  # Compare dates to check if the time between any two dates is ever greater than the given window
+  def includes_inactive?(dates, inactivity_window)
+    dates.each_with_index do |date, index|
       unless index == pub_dates.size - 1
+        # Compare dates with simple operand to get simple number of days difference
         time_between = date - pub_dates[index + 1]
         if time_between > inactivity_window
           return true
